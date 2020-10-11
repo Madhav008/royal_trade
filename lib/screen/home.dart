2abb5777cf5f170133f13bb607601b9a7321d1ac
Hari Om <58305594+Madhav008@users.noreply.github.com>
2020-10-11 16:20:33 +0530

    color changes

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -4,85 +4,84 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 20, 52, 1),
       drawer: Container(),
       appBar: AppBar(
         centerTitle: true,
         title: Text("Royal Trade"),
       ),
-      body:Stack(
-            children: [
-      Positioned(
-        top: 10,
-        left: 10,
-        right: 10,
-        child: Card(
-              child: Container(
-                height: 100,
-                child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Binary Signal"),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                      icon: Icon(Icons.access_time), onPressed: null),
-                )
-            ],
-          ),
-              )),
-      ),
-      Positioned(
-        top: 120,
-        left: 10,
-        right: 10,
-        child: Card(
-            child: Container(
-              height: 100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Vip Signal"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: IconButton(
-                    icon: Icon(Icons.access_time), onPressed: null),
-              )
-            ],
-          ),
-        )),
-      ),
-      Positioned(
-        top: 230,
-        left: 10,
-        right: 10,
-        child: Card(
-            child: Container(
-              height: 100,
-              child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Forex Signal"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: IconButton(
-                    icon: Icon(Icons.access_time), onPressed: null),
-              )
+                  child: Card(
+                     color: Color.fromRGBO(16, 41, 87, 1),
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Forex Signal"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                            icon: Icon(Icons.access_time), onPressed: null),
+                      )
+                    ],
+                  )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                     color: Color.fromRGBO(16, 41, 87, 1),
+                      child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("Vip Signal"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: IconButton(
+                              icon: Icon(Icons.access_time), onPressed: null),
+                        )
+                      ],
+                    ),
+                  )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                     color: Color.fromRGBO(16, 41, 87, 1),
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Forex Signal"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                            icon: Icon(Icons.access_time), onPressed: null),
+                      )
+                    ],
+                  )),
+                ),
+              ],
+            )
           ],
         ),
-            )),
       ),
-            ],
-          ),
-    
     );
   }
 }

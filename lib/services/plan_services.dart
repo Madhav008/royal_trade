66c2abbd6c5948ccdf1b4024da057710d6c1499a
Hari Om <58305594+Madhav008@users.noreply.github.com>
2020-11-11 23:40:51 +0530

    plans backend done

--- lib/services/plan_services.dart
+++ lib/services/plan_services.dart
@@ -12,4 +12,12 @@ class PlansFirestore {
   Future<Subscription> getPlans(String id ) {
     return  _db.collection('Subscription').doc(id).get().then((value) => Subscription.fromFirestore(value.data()));
   }
+
+  Future<Subscription> addBinaryPlans(Subscription plans) {
+    return _db.collection('BinarySubscription').doc().set(plans.toMap());
+  }
+
+  Future<Subscription> getBinaryPlans(String id ) {
+    return  _db.collection('BinarySubscription').doc(id).get().then((value) => Subscription.fromFirestore(value.data()));
+  }
 }
